a0f1cc4977a57a4ef928074ce774c195a368faff
Akash Kurdekar <akashkurdekar39@gmail.com>
2025-04-04 01:30:24 +0530

    changed the media of navbar and section.css

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
 
       <main>
         <HeroSection />
-        {/* <About /> */}
+        <About />
         {/* <Skills/> */}
         {/* <Experience/> */}
         {/* <Contact/> */}
